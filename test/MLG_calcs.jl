788298582a7adf84fa0351d1daa143036549965d
Fernando Penaranda <fernandopenaranda@github.com>
2025-10-01 18:56:21 +0200

    + boolean test functions

--- test/MLG_calcs.jl
+++ test/MLG_calcs.jl
@@ -1,3 +1,12 @@
+function does_not_throw(f::Function, args...)
+    try
+        f(args...)
+        return true   # no error
+    catch
+        return false  # error occurred
+    end
+end
+
 @testset "MLG_calcs" begin
     cp = Computation_presets(
         xbounds = [0, 2π/√3],
@@ -6,7 +15,6 @@
         broadening = 0.01,
         evals = 1e1
     )
-    
     μ = 0
     h(q) = Presets.MLG_hamiltonian(μ, q) # system specific
     nabla_h(q) = Presets.MLG_nabla(q) 
@@ -15,7 +23,7 @@
     sigma_ij_presets = σij_presets(:x, :x, h,
         nabla_h, cp)
 
-    @test dos(dos_presets)
-    @test jdos(jdos_presets)
-    @test linear_optical_conductivity(sigma_ij_presets)
+    @test does_not_throw(dos, dos_presets)
+    @test does_not_throw(jdos, jdos_presets)
+    @test does_not_throw(linear_optical_conductivity, sigma_ij_presets)
 end

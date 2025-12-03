cddbc0f9df1dd67561c627ecca86b83d7e90dfda
Fernando Penaranda <fernandopenaranda@github.com>
2025-12-03 14:57:34 +0100

    fix

--- src/drude_conductivity.jl
+++ src/drude_conductivity.jl
@@ -10,7 +10,7 @@ drude_conductivity(p::Drude_presets) = drude_conductivity(p.h, p.dhi, p.T, p.τ,
         p.computation.xbounds, p.computation.ybounds, p.computation.evals)
 
 function drude_conductivity(h, dh, T, τ, xbounds, ybounds, evals; rel_tol = 1e-5, abs_tol = 0)
-    C = 2π * τ * ħ_ev_s/ ang_to_m^2 #wait do we need a correction in here
+    C = 2π * τ * ħ_ev_s/ (2π*ang_to_m)^2 #wait do we need a correction in here
     integrand(q) = real(k_in_plane_bindependent_conductivity(h(q), dh(q), T))
     val = bz_integration_transport(integrand, xbounds, ybounds, evals; rel_tol = rel_tol, abs_tol = abs_tol)
     return C * val

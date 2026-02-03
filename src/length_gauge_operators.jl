91130d4c092eb7f267a62fae457ac01e3cc79c08
Fernando Penaranda <fernandopenaranda@github.com>
2026-02-03 18:30:32 +0100

    1e3 Temperature

--- src/length_gauge_operators.jl
+++ src/length_gauge_operators.jl
@@ -112,7 +112,9 @@ function fn(ϵn, μ, T)
     if T == 0
         return ifelse(ϵn < μ, 1.0, 0.0)
     else
-        return 1/(exp((ϵn - μ)/(kB * T)) + 1)
+        # return 1/(exp((ϵn - μ)/(kB * T)) + 1) # For RHG
+        return 1/(exp((ϵn - μ)/(1e3*kB * T)) + 1)
+
     end
 end
 # first derivative with respect to E
